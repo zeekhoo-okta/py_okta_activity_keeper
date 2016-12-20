3b902d2cfc4577a1ced2b65d1d7ad6babcd3513e
zeekhoo-okta <zeekhoo@gmail.com>
2016-12-19 17:14:02 -0800

    moved debug setting to env variable

--- time_app/settings.py
+++ time_app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '***REMOVED***'
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', 'activity-timekeeper-dev.us-west-2.elasticbeanstalk.com', 'alwaysaasure.com', 'www.alwaysaasure.com']
 
